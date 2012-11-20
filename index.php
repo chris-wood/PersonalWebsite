c05425c77b54354c975a7b228a062f6975336810
Christopher Wood <christopherwood07@gmail.com>
2012-11-19 20:13:57 -0500

    Initial commit.

---
+++ index.php
@@ -0,0 +1,133 @@
+<!DOCTYPE html>
+<html>
+<head>
+<script src="js/jquery-1.8.3.min.js"></script>
+<meta charset=utf-8 />
+<link href="css/main.css" rel="stylesheet" media="projection, screen">
+<link href="css/print.css" rel="stylesheet" media="print">
+<title>Christopher A. Wood</title>
+</head>
+  
+<body>
+  <div id="outerWrapper">
+    <div id="nav">
+      <figure id="portrait">
+        <a href="#"><span><img src="imgs/portrait.jpg" width="150" height="140" 
+        alt="portrait of Christopher"></span></a>
+      </figure><!-- end #portrait -->
+      
+      <nav>
+        <h2 class="hide">navigation</h2>
+        
+        <ol>
+          <li><a href="#about">About</a></li>
+          <li><a href="#edu">Education</a></li>
+          <li><a href="#interests">Interests</a></li>
+          <li><a href="#positions">Current Positions</a></li>
+          <li><a href="#experience">Work Experience</a></li>
+          <li><a href="#training">Additional Training</a></li>
+          <li><a href="#projects">Highlighted Projects</a></li>
+          <li><a href="#skills">Skills</a></li>
+          <li><a href="#publications">Publications</a></li>
+          <!--<li><a href="#articles">Opinion Articles</a></li>-->
+          <li><a href="#presentations">Presentations</a></li>
+          <!--<li><a href="#press">Press</a></li>-->
+          <li><a href="#honors">Honors</a></li>
+          <!--<li><a href="#mentoring">Mentoring</a></li>-->
+          <li><a href="#service">Service</a></li>
+          <li><a href="#teaching">Teaching</a></li>
+          <li><a href="./docs/caw_cv.pdf" title="Curriculum Vitae">Curriculum Vitae</a></li>
+          <!--<li><a href="research-statement.pdf" title="Research Statement">Research Statement</a></li>
+          <li><a href="teaching-statement.pdf" title="Teaching Statement">Teaching Statement</a></li>-->
+        </ol>
+      </nav>
+    </div><!-- end #nav -->
+    
+    <div id="contentWrapper">
+      <section id="contactInfo">
+        <h2 class="hide">contact info</h2>
+        
+        <!-- TODO: need a portrait -->
+        <img src="imgs/portrait-print.jpg" width="150" height="70" alt="portrait of Andrés">
+        
+        <address>
+          <ul itemscope itemtype="http://schema.org/Person">
+            <li><span itemprop="email">caw4567@rit.edu</span></li>
+            <li><span itemprop="phone">315-806-5939</span></li>
+          </ul>
+        </address>
+      </section><!-- end #contactInfo -->
+      
+      <h1 style="text-align:center;"><span itemprop="name">Christopher A. Wood</span></h1>
+      
+      <!-- start the content here!-->
+      <section id="about" itemscope itemtype="http://schema.org/Person">
+        TODO
+      </section>
+
+      <section id="edu">
+        <h2>Education</h2>
+        
+        <ul>
+          <li>
+            <h3><b>Rochester Institute of Technology</b> <span class="floatRight">2013</span></h3>
+            <p><b>MS</b> in Computer Science</p>
+            <p>Thesis: TODO</p>
+            <p>Committee: Mitchel Resnick, Yochai Benkler, Rob Miller</p>
+          </li>
+          
+          <li>
+            <h3><b>Rochester Institute of Technology</b> <span class="floatRight">2012</span></h3>
+            <p><b>BS</b> in Computer Science and Software Engineering</p>
+            <p> elective courses here </p>
+          </li>
+        </ul>
+      </section>
+
+      <section id="interests">
+        TODO
+      </section>
+
+      <section id="positions">
+        TODO
+      </section>
+
+      <section id="experience">
+        TODO
+      </section>
+  
+      <section id="training">
+        TODO
+      </section>
+
+      <section id="projects">
+        TODO
+      </section>
+
+      <section id="skills">
+        TODO
+      </section>
+
+      <section id="publications">
+        TODO
+      </section>
+
+      <section id="presentations">
+        TODO
+      </section>
+
+      <section id="honors">
+        TODO
+      </section>
+
+      <section id="service">
+        TODO
+      </section>
+
+      <section id="teaching">
+        TODO
+      </section>
+    </div> <!-- end #contentWrapper -->
+  </div> <!-- end #outerWrapper -->
+</body>
+</html>
