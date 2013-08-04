ff315037fe2addd174c3db9fb38e7d47696af5d8
Christopher Wood <christopherwood07@gmail.com>
2013-08-04 13:48:05 -0700

    More updates to the website

--- research/index.php
+++ research/index.php
@@ -13,7 +13,7 @@
     <meta name="author" content="">
 
     <!-- Le styles -->
-    <link href="./css/bootstrap.css" rel="stylesheet">
+    <link href="./css/bootstrap.min.css" rel="stylesheet">
     <style type="text/css">
       body {
         padding-top: 20px;
@@ -51,7 +51,7 @@
         margin-top: 28px;
       }
     </style>
-    <link href="./css/bootstrap-responsive.css" rel="stylesheet">
+    <!-- <link href="./css/bootstrap-responsive.css" rel="stylesheet"> -->
 
     <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
     <!--[if lt IE 9]>
@@ -77,35 +77,9 @@
   <body>
 
     <div class="container">
-
-      <div class="navbar navbar-static-top">
-        <p class="navbar-brand">Research</p>
-        <p class="navbar-text pull-right">Home: <a target="_blank" href="http://www.christopher-wood.com/" class="navbar-link">Christopher A. Wood</a></p>
-      </div>
-
-      <div class="row-fluid marketing">
-        <div class="span12">
-          <h3>Summary</h3>
-          <p>
-            I <emph>try</emph> to maintain a collection of my research progress and results on this page. I'm a complete advocate of open, collaborative research, and believe in the honor code among scientists. That is, I won't steal your work, and I expect the same courtesy.
-          </p>
-          <p> 
-            Select one of the topics below to navigate to more specific details.
-          </p>
-            <div class="row" style="text-align:center;">
-              <div class="span2"><a class="btn btn-success" href="#">Cryptography</a></div>
-              <div class="span2"><a class="btn btn-success" href="#">Security and Privacy</a></div>
-              <div class="span2"><a class="btn btn-success" href="#">Computational Graph Theory</a></div>
-              <div class="span2"><a class="btn btn-success" href="#">Combinatorics</a></div>
-            </div>
-          </p>
-        </div>
-      </div>
-
-      <div class="container">
         <div class="tabbable tabs-left">
           <ul class="nav nav-tabs">
-            <li><a href="#home" data-toggle="tab">Home</a></li>
+            <li><a href="#home" data-toggle="tab">Overview</a></li>
             <li><a href="#crypto" data-toggle="tab">Cryptography</a></li>
             <li><a href="#snp" data-toggle="tab">Security and Privacy</a></li>
             <li><a href="#cgt" data-toggle="tab">Computational Graph Theory</a></li>
@@ -114,9 +88,12 @@
           </ul>
         <div class="tab-content">
           <div class="tab-pane" id="home">
-            <h3>Home</h3> 
-            <?php include_once('./home.htm'); ?>
-          </div>
+            <h3>Overview</h3> 
+            <p>
+              I <emph>try</emph> to maintain a collection of my research progress and results on this page. 
+              I'm a complete advocate of open, collaborative research, and believe in the honor code among scientists. 
+              That is, I won't steal your work, and I expect the same courtesy.
+            </p>
           <div class="tab-pane" id="crypto">
             <h3>Cryptography</h3> 
             <?php include_once('./crypto.htm'); ?>
@@ -144,19 +121,14 @@
     <!-- Le javascript
     ================================================== -->
     <!-- Placed at the end of the document so the pages load faster -->
-    <script src="./js/jquery.js"></script>
-    <script src="./js/bootstrap-transition.js"></script>
-    <script src="./js/bootstrap-alert.js"></script>
-    <script src="./js/bootstrap-modal.js"></script>
-    <script src="./js/bootstrap-dropdown.js"></script>
-    <script src="./js/bootstrap-scrollspy.js"></script>
-    <script src="./js/bootstrap-tab.js"></script>
-    <script src="./js/bootstrap-tooltip.js"></script>
-    <script src="./js/bootstrap-popover.js"></script>
-    <script src="./js/bootstrap-button.js"></script>
-    <script src="./js/bootstrap-collapse.js"></script>
-    <script src="./js/bootstrap-carousel.js"></script>
-    <script src="./js/bootstrap-typeahead.js"></script>
+    <script src="./js/jquery-1.10.2.min.js"></script>
+    <script src="./js/bootstrap.min.js"></script>
 
+     <script>
+        var _gaq=[['_setAccount','UA-XXXXX-X'],['_trackPageview']];
+        (function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
+        g.src=('https:'==location.protocol?'//ssl':'//www')+'.google-analytics.com/ga.js';
+        s.parentNode.insertBefore(g,s)}(document,'script'));
+    </script>
   </body>
 </html>
