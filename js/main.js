749d12175e8cfbf26eb1db57ddb0ab9c3d759183
Christopher Wood <christopherwood07@gmail.com>
2015-02-18 10:20:13 -0800

    Major cleanup

--- js/main.js
+++ js/main.js
@@ -1,11 +1,60 @@
-;(function(){
+// define sets and set set intersections
+var sets = [
+	{label: "Security & Privacy", size: 20}, 
+	{label: "Content-Centric Networking", size: 20},
+	{label: "Theory & Formal Methods", size: 20}
+	];
+var overlaps = [
+	{sets: [0,1], size: 2},
+	{sets: [0,2], size: 2},
+	{sets: [1,2], size: 2}
+	];
 
-			// Menu settings
-			$('#menuToggle, .menu-close').on('click', function(){
-				$('#menuToggle').toggleClass('active');
-				$('body').toggleClass('body-push-toleft');
-				$('#theMenu').toggleClass('menu-open');
-			});
+// get positions for each set
+sets = venn.venn(sets, overlaps);
 
+// draw the diagram in the 'simple_example' div
+var diagram = venn.drawD3Diagram(d3.select("#research-areas"), sets, 500, 500);
+diagram.nodes
+    .on("mouseover", function(d, i) {
+        var node = d3.select(this).transition();
+        node.select("text").style("font-size", "18px");
+    })
+    .on("mouseout", function(d, i) {
+        var node = d3.select(this).transition();
+        node.select("text").style("font-size", "16px");
+    });
 
-})(jQuery)
+var colours = d3.scale.category10();
+diagram.circles.style("fill-opacity", .8)
+	.style("stroke-opacity", 0)
+	.style("fill", function(d,i) { return colours(i); })
+	.style("stroke", function(d,i) { return colours(i); });
+diagram.text.style("fill", "white");
+
+var app = angular.module("caw-site", []);
+app.controller('DailyGrindController', ['$scope', '$http', function($scope, $http) {
+
+	$scope.lastTracks = [];
+
+	var apiKey = "5a328a61c5b345719cd2faf016043a81";
+	var username = "chriswood4567";
+	var lastFmUrl = "http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=" + username + "&api_key=" + apiKey + "&format=json";
+	$http.get(lastFmUrl).
+		success(function(data, status, headers, config) {
+			console.log(data);
+			for (var i = 0; i < data.recenttracks.track.length && i < 5; i++) {
+				var trackData = data.recenttracks.track[i];
+				$scope.lastTracks.push({
+					"name" : trackData.name,
+					"artist" : trackData.artist,
+					"album" : trackData.album
+				})
+			}
+			console.log($scope.lastTracks);
+		}).
+		error(function(data, status, headers, config) {
+			$scope.lastTracks = [];
+		});
+
+}]);
--- old/index.php
+++
@@ -1,3 +0,0 @@
-<?php
-  include_once "index.html"; // this will include a.php
-?>
